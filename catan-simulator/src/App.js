9c0bdc070812cc1cecf7483458f31ae0c2fa92fb
William Peter Åredal <williamaredal@hotmail.com>
2021-04-01 00:03:51 +0200

    Initial commit

---
+++ catan-simulator/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>
+          Website for viewing outcomes of simulated winning strategies in the game catan.
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
