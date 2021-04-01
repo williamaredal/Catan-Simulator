0d269f98bec14f72b15c2a6d280cc2e0b220fb86
William Peter Åredal <williamaredal@hotmail.com>
2021-04-01 19:56:40 +0200

    Added tailwind to project

--- catan-simulator/src/App.js
+++ catan-simulator/src/App.js
@@ -3,9 +3,26 @@ function App() {
   return (
     <div>
       <header>
-        <h1>
-          Website for viewing outcomes of simulated winning strategies in the game catan.
-        </h1>
+
+        <table>
+
+          <caption>Website for viewing outcomes of simulated winning strategies in the game catan.</caption>
+
+          <tr>
+            <th>header1</th>
+            <th>header2</th>
+            <th>header3</th>
+          </tr>
+
+          <tbody>
+            <tr>
+              <td>column1</td>
+              <td>column2</td>
+              <td>column3</td>
+            </tr>
+          </tbody>
+
+        </table>
       </header>
     </div>
   );
