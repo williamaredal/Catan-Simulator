a8a005b8a3926daade77a0c29c15ce75a3aea224
William Peter Åredal <williamaredal@hotmail.com>
2021-04-23 21:06:43 +0200

    Added sorting function for virt table. Strange bug

--- catan-simulator/src/VirtualizedTable.js
+++ catan-simulator/src/VirtualizedTable.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import TableContainer from '@material-ui/core/TableContainer';
 import Paper from '@material-ui/core/Paper';
-import { Table, Column } from 'react-virtualized';
+import { Table, Column, SortDirection } from 'react-virtualized';
 import './customStyles.css'
 import ModalCard from './ModalCard';
 
@@ -9,6 +9,10 @@ export default function VirtualizedTable({dataRows}) {
 
     const [modalState, setModal] = useState(false);
     const [modalData, setModalData] = useState(0);
+
+    const [sortBy, setSortBy] = useState('index');
+    const [sortDirection, setSortDirection] = useState(SortDirection.ASC);
+    const [sortedList, setSortedList] = useState(dataRows)
 
     const openCard = (index) => {
         setModalData(dataRows[index])
@@ -18,6 +22,28 @@ export default function VirtualizedTable({dataRows}) {
     const closeCard = () => {
         setModal(false)
     }
+
+    const sortColumn = (currentSortDirection, currentSortBy) => {
+        setSortedList(0);
+        setSortBy(currentSortBy);
+        if ((currentSortBy === sortBy) && (sortDirection === SortDirection.ASC)) {
+            setSortBy(currentSortBy);
+            setSortDirection(SortDirection.DESC);
+        }
+
+        else if ((currentSortBy === sortBy) && (sortDirection === SortDirection.DESC)) {
+            setSortBy(currentSortBy);
+            setSortDirection(SortDirection.ASC);
+        }
+
+        else {
+            setSortBy(currentSortBy);
+            setSortDirection(SortDirection.ASC);
+        }
+
+        const sorted = (sortDirection === SortDirection.DESC) ? sortedList.sort((a, b) => b[currentSortBy] - a[currentSortBy]) : sortedList.sort((a, b) => a[currentSortBy] - b[currentSortBy]);
+        setSortedList(sorted);
+      };
 
 
     return (
@@ -30,6 +56,7 @@ export default function VirtualizedTable({dataRows}) {
             rowCount={dataRows.length}
             rowGetter={({index}) => dataRows[index]}
             onRowClick={(row) => openCard(row.index)}
+            sort={(event) => {sortColumn(event.sortDirection, event.sortBy)}}
             >
                 <Column width={135} label="Simulation" dataKey="simulation"/>
                 <Column width={135} label="victoryPoints" dataKey="victoryPoints"/>
